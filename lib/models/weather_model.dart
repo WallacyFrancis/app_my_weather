3b0c4429533b9f117766b687fa0aff329d147bd2
WallacyFrancis <wallacy_francis@hotmail.com>
2025-06-19 16:29:28 -0300

    implementa modelo de dados Weather com suporte a JSON e igualdade de instâncias

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,1 +1,27 @@
+import 'package:equatable/equatable.dart';
 
+class Weather extends Equatable {
+  final String cityName;
+  final double temperature;
+  final String description;
+  final String iconCode;
+
+  const Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.description,
+    required this.iconCode,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'],
+      temperature: json['main']['temp'].toDouble(),
+      description: json['weather'][0]['description'],
+      iconCode: json['weather'][0]['icon'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [cityName, temperature, description, iconCode];
+}
